345d505784224375171122d2b8fd2be650618ab3
Yair Rodriguez <yrodriguez@invistics.com>
2025-08-07 11:45:56 -0600

    Initial commit

---
+++ SemanticKernelExample/Program.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+
+var modelId = string.Empty;
+var endpoint = string.Empty;
+var apiKey = string.Empty;
+
+var builder = Kernel.CreateBuilder();
+builder.AddAzureOpenAIChatClient(modelId, endpoint, apiKey);
+
+Console.WriteLine("Hello, World!");
